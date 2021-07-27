64c29d309299ae9d11be7917496d9a56a50eac28
junjunchaochao <1141303883@qq.com>
2021-07-27 21:10:51 +0800

    update view/form

---
+++ src/views/form/delete.vue
@@ -0,0 +1,69 @@
+<template>
+  <div class="formDelete">
+    <el-button  @click="open1" type="primary">添加</el-button>
+    <el-button  @click="open" type="danger">删除</el-button>
+  </div>
+</template>
+
+<script>
+  export default {
+    methods: {
+      open() {
+        this.$confirm('此操作将永久删除该文件, 是否继续?', '提示', {
+          confirmButtonText: '确定',
+          cancelButtonText: '取消',
+          type: 'warning'
+        }).then(() => {
+          this.$message({
+            type: 'success',
+            message: '删除成功!'
+          });
+        }).catch(() => {
+          this.$message({
+            type: 'info',
+            message: '已取消删除'
+          });          
+        });
+      },
+      open1(){
+        const h = this.$createElement;
+        this.$msgbox({
+          title: '添加用户',
+          message: h('p', null, [
+            h('span', null, '内容可以是 '),
+            h('i', { style: 'color: teal' }, 'VNode')
+          ]),
+          showCancelButton: true,
+          confirmButtonText: '确定',
+          cancelButtonText: '取消',
+          beforeClose: (action, instance, done) => {
+            if (action === 'confirm') {
+              instance.confirmButtonLoading = true;
+              instance.confirmButtonText = '执行中...';
+              setTimeout(() => {
+                done();
+                setTimeout(() => {
+                  instance.confirmButtonLoading = false;
+                }, 300);
+              }, 3000);
+            } else {
+              done();
+            }
+          }
+        }).then(action => {
+          this.$message({
+            type: 'info',
+            message: 'action: ' + action
+          });
+        });
+      }
+    }
+  }
+</script>
+
+<style scoped>
+  .formDelete{
+    margin: 20px 10px;
+
+  }
+  </style>
--- src/views/form/index.vue
+++ src/views/form/index.vue
@@ -1,85 +1,138 @@
 <template>
-  <div class="app-container">
-    <el-form ref="form" :model="form" label-width="120px">
-      <el-form-item label="Activity name">
-        <el-input v-model="form.name" />
-      </el-form-item>
-      <el-form-item label="Activity zone">
-        <el-select v-model="form.region" placeholder="please select your zone">
-          <el-option label="Zone one" value="shanghai" />
-          <el-option label="Zone two" value="beijing" />
-        </el-select>
-      </el-form-item>
-      <el-form-item label="Activity time">
-        <el-col :span="11">
-          <el-date-picker v-model="form.date1" type="date" placeholder="Pick a date" style="width: 100%;" />
-        </el-col>
-        <el-col :span="2" class="line">-</el-col>
-        <el-col :span="11">
-          <el-time-picker v-model="form.date2" type="fixed-time" placeholder="Pick a time" style="width: 100%;" />
-        </el-col>
-      </el-form-item>
-      <el-form-item label="Instant delivery">
-        <el-switch v-model="form.delivery" />
-      </el-form-item>
-      <el-form-item label="Activity type">
-        <el-checkbox-group v-model="form.type">
-          <el-checkbox label="Online activities" name="type" />
-          <el-checkbox label="Promotion activities" name="type" />
-          <el-checkbox label="Offline activities" name="type" />
-          <el-checkbox label="Simple brand exposure" name="type" />
-        </el-checkbox-group>
-      </el-form-item>
-      <el-form-item label="Resources">
-        <el-radio-group v-model="form.resource">
-          <el-radio label="Sponsor" />
-          <el-radio label="Venue" />
-        </el-radio-group>
-      </el-form-item>
-      <el-form-item label="Activity form">
-        <el-input v-model="form.desc" type="textarea" />
-      </el-form-item>
-      <el-form-item>
-        <el-button type="primary" @click="onSubmit">Create</el-button>
-        <el-button @click="onCancel">Cancel</el-button>
-      </el-form-item>
-    </el-form>
+  
+  <div class="formTabel">
+    <Delete></Delete>
+    <el-table
+    ref="multipleTable"
+    :data="tableData"
+    border
+    tooltip-effect="dark"
+    style="width: 100%"
+    >
+    <el-table-column
+      type="selection"
+      width="55">
+    </el-table-column>
+    <el-table-column
+      fixed
+      prop="id"
+      label="用户id"
+      width="100">
+    </el-table-column>
+    <el-table-column
+      prop="name"
+      label="用户名"
+      width="220">
+    </el-table-column>
+    <el-table-column
+      prop="sexy"
+      label="性别"
+      width="120">
+    </el-table-column>
+    <el-table-column
+      prop="call"
+      label="电话"
+      width="200">
+    </el-table-column>
+    <el-table-column
+      prop="address"
+      label="邮箱"
+      width="300">
+    </el-table-column>
+    <el-table-column
+      prop="date"
+      label="创建日期"
+      width="200">
+    </el-table-column>
+    <el-table-column
+      prop="date1"
+      label="更新日期"
+      width="200">
+    </el-table-column>
+    <el-table-column
+      fixed="right"
+      label="操作"
+      width="100">
+      
+      <template slot-scope="scope">
+        <el-button @click="handleClick(scope.row)" type="text" size="small">查看</el-button>
+        <el-button type="text" size="small">编辑</el-button>
+      </template>
+    </el-table-column>
+  </el-table>
+  
   </div>
+
+  
+
 </template>
 
 <script>
-export default {
-  data() {
-    return {
-      form: {
-        name: '',
-        region: '',
-        date1: '',
-        date2: '',
-        delivery: false,
-        type: [],
-        resource: '',
-        desc: ''
+  import Delete from './delete.vue';
+
+  export default {
+    methods: {
+      handleClick(row) {
+        console.log(row);
       }
-    }
-  },
-  methods: {
-    onSubmit() {
-      this.$message('submit!')
     },
-    onCancel() {
-      this.$message({
-        message: 'cancel!',
-        type: 'warning'
-      })
+    data() {
+      return {
+        tableData: [{
+          id: '001',
+          name: '王小虎',
+          sexy: '男',
+          call: '15255669988',
+          address: '1111111@qq.com',
+          date: '2021-07-27',
+          date1:'2021-07-27'
+        }, {
+          id: '002',
+          name: '王小虎',
+          sexy: '男',
+          call: '15255669918',
+          address: '1111111@qq.com',
+          date: '2021-07-27',
+          date1:'2021-07-27'
+        }, {
+          id: '003',
+          name: '王小虎',
+          sexy: '男',
+          call: '15255669998',
+          address: '1111111@qq.com',
+          date: '2021-07-27',
+          date1:'2021-07-27'
+        }, {
+          id: '004',
+          name: '王小虎',
+          sexy: '女',
+          call: '15255662288',
+          address: '1111111@qq.com',
+          date: '2021-07-27',
+          date1:'2021-07-27'
+        }]
+      }
+    },
+    components:{
+      Delete
     }
+    // methods:{
+    //   toggleSelection(rows) {
+    //     if (rows) {
+    //       rows.forEach(row => {
+    //         this.$refs.multipleTable.toggleRowSelection(row);
+    //       });
+    //     } else {
+    //       this.$refs.multipleTable.clearSelection();
+    //     }
+    //   }
+    // }
   }
-}
 </script>
 
-<style scoped>
+<!-- <style scoped>
 .line{
   text-align: center;
 }
-</style>
+</style> -->
 
