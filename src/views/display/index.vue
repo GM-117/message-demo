8a49341d7643764047cbcc201d705377be4647f5
monze0117 <972983546@qq.com>
2021-07-30 09:05:36 +0800

    7.30 edit display/index by gm

--- src/views/display/index.vue
+++ src/views/display/index.vue
@@ -3,52 +3,78 @@
     <div class="select_button">
       <el-form size="mini" :inline="true">
         <el-form-item>
-          <el-button type="primary" @click="B_search">查询</el-button>
+          <el-button
+            type="primary"
+            plain
+            icon="el-icon-search"
+            @click="Btn_search"
+            >查询</el-button
+          >
         </el-form-item>
         <el-form-item>
-          <el-button type="primary" @click="B_reset">重置</el-button>
+          <el-button
+            type="primary"
+            plain
+            icon="el-icon-refresh-left"
+            @click="Btn_reset('formData')"
+            >重置</el-button
+          >
         </el-form-item>
       </el-form>
     </div>
     <div class="select_input">
-      <el-form :inline="true" label-width="80px" size="mini">
-        <el-form-item label="工单主题">
-          <el-input v-model="formData.theme" placeholder="请输入工单主题" clearable></el-input>
+      <el-form
+        :inline="true"
+        label-width="80px"
+        size="mini"
+        :model="formData"
+        ref="formData"
+      >
+        <el-form-item prop="theme" label="工单主题">
+          <el-input
+            v-model="formData.theme"
+            placeholder="请输入工单主题"
+            clearable
+          ></el-input>
         </el-form-item>
-        <el-form-item label="编号" size="mini">
-          <el-input v-model="formData.id" placeholder="请输入编号" clearable></el-input>
+        <el-form-item prop="id" label="编号" size="mini">
+          <el-input
+            v-model="formData.id"
+            placeholder="请输入编号"
+            clearable
+          ></el-input>
         </el-form-item>
-        <el-form-item label="受理人" size="mini">
+        <el-form-item prop="accepted" label="受理人" size="mini">
           <el-select v-model="formData.accepted" placeholder="请选择">
             <el-option label="admin" value="0"></el-option>
           </el-select>
         </el-form-item>
-        <el-form-item label="状态" size="mini">
+        <el-form-item prop="status.value" label="状态" size="mini">
           <el-select v-model="formData.status.value" placeholder="请选择">
             <el-option label="未处理" value="0"></el-option>
             <el-option label="处理中" value="1"></el-option>
             <el-option label="已完成" value="2"></el-option>
           </el-select>
         </el-form-item>
-        <el-form-item label="创建人" size="mini">
+        <el-form-item prop="founder.value" label="创建人" size="mini">
           <el-select v-model="formData.founder.value" placeholder="请选择">
             <el-option label="管理员" value="0"></el-option>
-            <el-option label="用户" value="1"></el-option>
+            <el-option label="普通用户" value="1"></el-option>
           </el-select>
         </el-form-item>
-        <el-form-item label="优先级" size="mini">
+        <el-form-item prop="priority.value" label="优先级" size="mini">
           <el-select v-model="formData.priority.value" placeholder="请选择">
             <el-option label="高" value="0"></el-option>
             <el-option label="中" value="1"></el-option>
             <el-option label="低" value="2"></el-option>
           </el-select>
         </el-form-item>
-        <el-form-item label="创建方式" size="mini">
+        <el-form-item prop="createWay" label="创建方式" size="mini">
           <el-select v-model="formData.createWay" placeholder="请选择">
             <el-option label="自动创建" value="0"></el-option>
           </el-select>
         </el-form-item>
-        <el-form-item label="创建时间" size="mini">
+        <el-form-item prop="createTime" label="创建时间" size="mini">
           <el-date-picker
             v-model="formData.createTime"
             type="date"
@@ -61,10 +87,21 @@
     </div>
 
     <div class="operator_button">
-      <el-button size="small" :loading="buttonLoading" type="primary"
-        >新建</el-button
+      <el-button
+        size="small"
+        :loading="buttonLoading"
+        type="primary"
+        @click="jumpToAdd"
+        icon="el-icon-plus"
+        >添加</el-button
       >
-      <el-button size="small" :loading="buttonLoading" type="danger"
+      <el-button
+        size="small"
+        :loading="buttonLoading"
+        type="danger"
+        icon="el-icon-close"
+        :disabled="multipleSelection.length === 0"
+        @click="selectDelete"
         >删除</el-button
       >
     </div>
@@ -77,11 +114,12 @@
       highlight-current-row
       height="500"
       stripe
+      @selection-change="handleSelectionChange"
     >
       <el-table-column type="selection" class="select"> </el-table-column>
       <el-table-column label="编号" width="95" align="center">
         <template slot-scope="scope">
-          {{ scope.$index + 1 }}
+          {{ scope.row.id }}
         </template>
       </el-table-column>
       <el-table-column label="处置动作" width="110" align="center">
@@ -132,9 +170,16 @@
           {{ scope.row.pageviews }}
         </template>
       </el-table-column>
-      <el-table-column label="操作" align="center" width="80">
-        <template>
+      <el-table-column label="操作" align="center" width="120">
+        <template slot-scope="scope">
           <el-button type="text" size="small">详情</el-button>
+          <el-button
+            type="text"
+            size="small"
+            @click="handleDelete(scope.$index, list)"
+            style="color: #f56c6c"
+            >移除</el-button
+          >
         </template>
       </el-table-column>
     </el-table>
@@ -171,11 +216,12 @@ export default {
           value: "",
         },
         createWay: "",
-        createTime:""
+        createTime: "",
       },
       list: null,
       listLoading: true,
       buttonLoading: false,
+      multipleSelection:[]
     };
   },
   created() {
@@ -190,28 +236,73 @@ export default {
         this.listLoading = false;
       });
     },
-    B_search() {
+    jumpToAdd() {
+      this.$router.push("/order/add");
+    },
+    handleSelectionChange(val){
+      console.log(val)
+      this.multipleSelection = val
+    },
+    selectDelete(){
+      for(let i = this.formData.length -1; i >= 0; i--){
+        let Data = this.formData;
+        const hasData = this.multipleSelection.find(index => index.id === Data.id)
+        if(hasData){
+          this.formData.splice(index,1)
+        }
+      }
+    },
+    handleDelete(index, rows) {
+      console.log(rows);
+      this.$confirm("此操作将永久删除本条消息, 是否继续?", "提示", {
+        confirmButtonText: "确定",
+        cancelButtonText: "取消",
+        type: "warning",
+      }).then(() => {
+          rows.splice(index, 1);
+          this.$message({
+            type: "success",
+            message: "删除成功!",
+          });
+        }).catch(() => {
+          this.$message({
+            type: "info",
+            message: "已取消删除",
+          });
+        });
+    },
+    Btn_search() {
       console.log("search");
       console.log(this.formData);
+      // if(this.formData.theme == ""){
+      //   this.$message({
+      //     message: '请查询必选项',
+      //     type: 'warning'
+      //   });
+      // }
     },
-    B_reset() {
+    Btn_reset(formName) {
       console.log("reset");
+      this.$refs[formName].resetFields();
     },
   },
 };
 </script>
 <style scoped>
 .operator_button {
-  margin-bottom: 10px;
+  margin: 10px 0;
 }
 .el-input {
+  width: 178px;
+}
+.el-select {
   width: 178px;
 }
 .select_input {
   width: 95%;
 }
 .select_button {
-  width: 150px;
+  width: 170px;
   float: right;
 }
 </style>
--- src/views/userCenter/userCenter.vue
+++ src/views/userCenter/userCenter.vue
@@ -56,23 +56,23 @@
 export default {
   data() {
     return {
-      tableData: [
-        {
-          name: "王小虎",
-          province: "上海",
-          city: "普陀区",
-          address: "上海市普陀区金沙江路 1518 弄",
-          zip: 200333,
-        },
+      // tableData: [
+      //   {
+      //     name: "王小虎",
+      //     province: "上海",
+      //     city: "普陀区",
+      //     address: "上海市普陀区金沙江路 1518 弄",
+      //     zip: 200333,
+      //   },
 
-        {
-          name: "王小虎",
-          province: "上海",
-          city: "普陀区",
-          address: "上海市普陀区金沙江路 1518 弄",
-          zip: 200333,
-        },
-      ],
+      //   {
+      //     name: "王小虎",
+      //     province: "上海",
+      //     city: "普陀区",
+      //     address: "上海市普陀区金沙江路 1518 弄",
+      //     zip: 200333,
+      //   },
+      // ],
     };
   },
 };
